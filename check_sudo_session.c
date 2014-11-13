5be4ceb264e7af525d7afccfcad7893f8d493253
Henri Verroken <henriverroken@gmail.com>
2014-11-13 17:15:46 +0100

    Implement check without user id check support

--- check_sudo_session.c
+++ check_sudo_session.c
@@ -1,26 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+#include "options.h"
 #include "process.h"
+#include "nagios_exit.h"
 
-int main() {
-    process_t **proc_list, *p;
-    size_t num_procs, i;
+void perform_check(options_t *options);
 
-    proc_list = process_copy_running(&num_procs);
-    if(num_procs == 0) {
-        printf("error: process_copy_running\n");
-        free(proc_list);
-        exit(2);
+int main(int argc, const char **argv) {
+    options_t options;
+
+    if(parse_argv(&options, argc, argv) != 0) {
+        print_usage(stderr, argv[0]);
+        exit(EXIT_CRITICAL);
+    }
+
+    perform_check(&options);
+
+    return 0;
+}
+
+void perform_check(options_t *options) {
+    size_t num_procs = 0, i;
+    process_t **list, *p;
+    process_t *warning = NULL, *critical = NULL;
+
+    list = process_copy_running(&num_procs);
+    if(list == NULL || num_procs == 0) {
+        nagios_critical("could not get running processes");
+        return;
     }
 
     for(i = 0; i < num_procs; i++) {
-        p = proc_list[i];
-        printf("(%p, %d, %s, %lld)\n", p, p->pid, p->comm,
-               (long long)p->running);
-        free(p);
-    }
-    free(proc_list);
+        p = list[i];
+        if(strcmp(p->comm, "sudo") != 0) {
+            continue;
+        }
 
-    return 0;
+        if((time_t)p->running > (time_t)options->critical.value) {
+            if(critical == NULL || critical->running < p->running) {
+                critical = p;
+            }
+        }
+
+        if((time_t)p->running > (time_t)options->warning.value) {
+            if(warning == NULL || warning->running < p->running) {
+                warning = p;
+            }
+        }
+    }
+
+    if(critical) {
+        nagios_critical("hanging session: %d seconds (pid: %d)",
+                        (int)critical->running, (int)critical->pid);
+    } else if(warning) {
+        nagios_warning("hanging session: %d seconds (pid: %d)",
+                       (int)warning->running, (int)warning->pid);
+    } else {
+        nagios_ok("no hanging sessions");
+    }
+
+    for(i= 0; i < num_procs; i++) {
+        free(list[i]);
+    }
+    free(list);
 }
